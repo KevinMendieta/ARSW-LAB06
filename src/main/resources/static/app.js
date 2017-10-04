ec5170376562871a5ca2f408ea99a1fa14c3d3aa
KevinMendieta <kevin.mendieta-p@mail.escuelaing.edu.co>
2017-10-04 16:30:10 -0500

    FirstCommit

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -36,9 +36,9 @@ var app = (function () {
         //subscribe to /topic/TOPICXX when connections succeed
         stompClient.connect({}, function (frame) {
             console.log('Connected: ' + frame);
-            stompClient.subscribe('/topic/TOPICXX', function (eventbody) {
-                
-                
+            stompClient.subscribe('/topic/newpoint', function (eventbody) {
+                var content = JSON.parse(eventbody.body);
+                alert("x: " + content.x + " y: "+ content.y);
             });
         });
 
@@ -59,8 +59,8 @@ var app = (function () {
             var pt=new Point(px,py);
             console.info("publishing point at "+pt);
             addPointToCanvas(pt);
-
             //publicar el evento
+            stompClient.send("/topic/newpoint", {}, JSON.stringify(pt));
         },
 
         disconnect: function () {
